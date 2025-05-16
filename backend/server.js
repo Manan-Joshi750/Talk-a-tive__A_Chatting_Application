06f18089bd0310b379d5e76cf0143c3717fcd54d
Manan Joshi <joshimanan265@gmail.com>
2025-05-16 20:46:16 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
+const dotenv = require("dotenv");
+dotenv.config(); // ✅ must be at the very top
+
 const express = require("express");
 const connectDB = require("./config/db");
-const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const path = require("path");
 
-dotenv.config();
-connectDB();
+connectDB(); // now MONGO_URI will be defined
 const app = express();
 
 app.use(express.json()); // to accept json data
